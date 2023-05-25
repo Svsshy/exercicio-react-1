54d4e84c80cf024543666f37598f6c7f7089ec05
Christian <christian.brasil@live.com>
2023-05-25 15:21:31 -0300

    Exercício Completo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
 function App() {
+  let noTitle = "Title"
+  let textHolder = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tempus nec magna nec malesuada. Etiam lorem urna, auctor quis dictum et, semper nec nisl. Sed gravida dui eget tortor malesuada sollicitudin. Integer eu massa placerat dolor ullamcorper consequat. Cras diam sapien, gravida vel velit non, condimentum condimentum arcu. Mauris ut placerat turpis. Vestibulum vel lacinia erat. Cras a sem velit. Vestibulum sed odio diam. In hac habitasse platea dictumst. Suspendisse eu velit euismod, laoreet ante sodales, vulputate lacus. Quisque tincidunt varius massa at imperdiet. In efficitur, nisl vitae consequat dapibus, sapien sem tincidunt arcu, ac dapibus turpis ipsum eget ante. In mollis massa sem, at viverra turpis convallis nec. Sed eu aliquam leo. Curabitur eget neque mi. Donec porta aliquam mauris, in ultricies erat tincidunt vel. Maecenas quis aliquet quam. Curabitur ac ultricies urna, aliquam iaculis nulla. Ut sagittis odio dui, vel volutpat libero pulvinar convallis. Integer auctor fringilla diam, a convallis orci placerat et. Vivamus tincidunt convallis urna, eu dapibus magna dignissim rhoncus. Nullam semper euismod tellus, quis ullamcorper libero congue eget. Maecenas id dolor ut nibh vehicula iaculis a ut quam. Maecenas convallis ornare iaculis. Integer at velit velit. Vestibulum sit amet tempor quam. Nulla facilisi. Maecenas at erat at risus volutpat aliquet ut in eros. "
+  const [titleState, setTitleState] = useState(noTitle)
+  const [textState, setTextState] = useState(textHolder)
+
+  const handleOnChangeTitle = (event) => {
+    setTitleState(event.target.value)
+  }
+
+  
+  const handleOnChangeText = (event) => {
+    setTextState(event.target.value)
+  }
+
   return (
     <div className="App">
       <div className="mainContainer">
@@ -10,22 +24,21 @@ function App() {
           <div className="inputContainer">
             <div className="inputWrapper">
               <label>Título do documento</label>
-              <input placeholder="Título" type="text" />
+              <input placeholder="Título" type="text" onChange={handleOnChangeTitle}/>
             </div>
             <div className="inputWrapper">
               <label>Texto do documento</label>
               <textarea rows="5" placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tempus nec magna nec malesuada. Etiam lorem urna, auctor quis dictum et, semper nec nisl. Sed gravida dui eget tortor malesuada sollicitudin. Integer eu massa placerat dolor ullamcorper consequat. Cras diam sapien, gravida vel velit non, condimentum condimentum arcu. Mauris ut placerat turpis. Vestibulum vel lacinia erat. Cras a sem velit. Vestibulum sed odio diam. In hac habitasse platea dictumst. Suspendisse eu velit euismod, laoreet ante sodales, vulputate lacus. Quisque tincidunt varius massa at imperdiet. In efficitur, nisl vitae consequat dapibus, sapien sem tincidunt arcu, ac dapibus turpis ipsum eget ante. In mollis massa sem, at viverra turpis convallis nec.
 Sed eu aliquam leo. Curabitur eget neque mi. Donec porta aliquam mauris, in ultricies erat tincidunt vel. Maecenas quis aliquet quam. Curabitur ac ultricies urna, aliquam iaculis nulla. Ut sagittis odio dui, vel volutpat libero pulvinar convallis. Integer auctor fringilla diam, a convallis orci placerat et. Vivamus tincidunt convallis urna, eu dapibus magna dignissim rhoncus. Nullam semper euismod tellus, quis ullamcorper libero congue eget. Maecenas id dolor ut nibh vehicula iaculis a ut quam. Maecenas convallis ornare iaculis. Integer at velit velit. Vestibulum sit amet tempor quam. Nulla facilisi. Maecenas at erat at risus volutpat aliquet ut in eros.</div>
-" type="textarea" />
+" type="textarea" onChange={handleOnChangeText}/>
             </div>
           </div>
         </div>
         <div className="preview">
           <h1>Preview</h1>
           <div className="previewContainer">
-            <h1>Título</h1>
-            <div className="previewText">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tempus nec magna nec malesuada. Etiam lorem urna, auctor quis dictum et, semper nec nisl. Sed gravida dui eget tortor malesuada sollicitudin. Integer eu massa placerat dolor ullamcorper consequat. Cras diam sapien, gravida vel velit non, condimentum condimentum arcu. Mauris ut placerat turpis. Vestibulum vel lacinia erat. Cras a sem velit. Vestibulum sed odio diam. In hac habitasse platea dictumst. Suspendisse eu velit euismod, laoreet ante sodales, vulputate lacus. Quisque tincidunt varius massa at imperdiet. In efficitur, nisl vitae consequat dapibus, sapien sem tincidunt arcu, ac dapibus turpis ipsum eget ante. In mollis massa sem, at viverra turpis convallis nec.
-Sed eu aliquam leo. Curabitur eget neque mi. Donec porta aliquam mauris, in ultricies erat tincidunt vel. Maecenas quis aliquet quam. Curabitur ac ultricies urna, aliquam iaculis nulla. Ut sagittis odio dui, vel volutpat libero pulvinar convallis. Integer auctor fringilla diam, a convallis orci placerat et. Vivamus tincidunt convallis urna, eu dapibus magna dignissim rhoncus. Nullam semper euismod tellus, quis ullamcorper libero congue eget. Maecenas id dolor ut nibh vehicula iaculis a ut quam. Maecenas convallis ornare iaculis. Integer at velit velit. Vestibulum sit amet tempor quam. Nulla facilisi. Maecenas at erat at risus volutpat aliquet ut in eros.</div>
+            <h1>{titleState}</h1>
+            <div className="previewText">{textState}</div>
           </div>
         </div>
 
